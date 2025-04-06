051794038c67ee91193d756ec9e36602866cfd7b
SreejaReddy414 <reddysreeja741@gmail.com>
2025-04-06 19:28:28 +0530

    changed index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const __dirname = dirname(__filename);
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 

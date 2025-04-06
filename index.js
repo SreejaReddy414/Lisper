0046572ebccf4c5fdd70907af537899a0af2e3cf
SreejaReddy414 <reddysreeja741@gmail.com>
2025-04-06 17:52:59 +0530

    modified index.js

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 import express from "express";
 import axios from "axios";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
-  res.render("home.ejs");
+  res.render("home");
 });
+
 app.get("/index", async (req, res) => {
   try {
     const result = await axios.get("https://secrets-api.appbrewery.com/random");
-    res.render("index.ejs", {
+    res.render("index", {
       secret: result.data.secret,
       user: result.data.username,
     });
   } catch (error) {
-    console.log(error.response.data);
+    console.log(error?.response?.data || error.message);
     res.status(500).send("Error fetching data");
   }
 });
 
 app.listen(port, () => {
-  console.log("Server is running on port 3000");
-  console.log("Go to http://localhost:3000");
+  console.log(`Server running at http://localhost:${port}`);
 });
